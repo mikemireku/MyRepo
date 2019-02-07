ffe1d4228e95fb15d7d4a6d554567803bb7aedb7
mikemireku <47335247+mikemireku@users.noreply.github.com>
2019-02-07 09:45:20 -0700

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,21 @@
 public class HelloWorld {
-	
-	// Documentation for main method
+
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+	}
+	
+	public static int getNumOfLoonies(int changeNeeded){
+		//return (changeNeeded%200)/100;
+		return 1;
+	}
+	
+	public static int getNumOfQuarters(int changeNeeded){
+		return ((changeNeeded%200)%100)/25;
+		//return 1;
+	}
+	
+	public static int getNumOfDimes(int changeNeeded){
+		return (changeNeeded%25)/10;
 	}
 
 }
